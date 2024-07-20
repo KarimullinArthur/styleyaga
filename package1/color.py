d1f9967aad54c0bbe8ce39869a1dc8b48400d302
Karimullin Arthur <KarimullinArthur@disroot.org>
2024-07-20 05:59:52 +0300

    Add decorator for link, and some

--- package1/color.py
+++ package1/color.py
@@ -1,3 +1,6 @@
+import enum
+
+
 RESET = '\x1b[0m'
 CSI = '\x1b['
 
@@ -11,7 +14,8 @@ CYAN = 6
 WHITE = 7
 
 
-class Color:
+class Color(enum.Enum):
+    reset = '\x1b[0m'
     black = '0'
     red = '1'
     green = '2'
@@ -22,22 +26,36 @@ class Color:
     white = '7'
 
 
-class Style:
+class Style(enum.Enum):
     bold = ';1m'
+    normal = ''
 
 
-class Layer:
+class Layer(enum.Enum):
     fore = '3'
     back = '4'
 
 
-def set_style(color: Color=Color.white, style: Style=None, layer: Layer=Layer.fore):
-    result = f'{CSI+layer+color}m'
-    if style:
-        result = result[:-1] + style
+def get_style(color: Color = Color.white, style: Style = Style.normal,
+              layer: Layer = Layer.fore) -> str:
+    if color == Color.reset:
+        return Color.reset.value
 
-    if color == RESET:
-        print(RESET, end='')
+    result = f'{CSI + layer.value + color.value}m'
+    if style.value:
+        result = result[:-1] + style.value
+
+    return result
+
+
+def set_style(color: Color = Color.white, style: Style = Style.normal,
+              layer: Layer = Layer.fore) -> None:
+    if color == Color.reset:
+        print(Color.reset.value, end='')
         return
+
+    result = f'{CSI + layer.value + color.value}m'
+    if style.value:
+        result = result[:-1] + style.value
 
     print(result, end='')
